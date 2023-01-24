aee4e392c02663590d9cfeaac3a7db18a8281248
stermak <7107798@mail.ru>
2023-01-24 21:11:19 +0300

    upgrade

---
+++ Task2/app/src/main/java/ermak/production/task2/presentation/MainActivity.kt
@@ -0,0 +1,23 @@
+package ermak.production.task2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import ermak.production.task2.R
+import ermak.production.task2.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val animationView = binding.ltAnimation
+        animationView.setAnimation(R.raw.animation)
+        animationView.playAnimation()
+    }
+}
